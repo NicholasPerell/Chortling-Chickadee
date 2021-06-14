88092cb8b3dc9196f46ea5f40344c29963fdbc57
Ryan Koczalka <ryan.koczalka@mymail.champlain.edu>
2021-06-14 02:29:29 -0400

    Finished all behaviors, have to bugfix wall collisions and need to figure out projectiles

--- Assets/Scripts/SquidellyController.cs
+++ Assets/Scripts/SquidellyController.cs
@@ -8,8 +8,20 @@ public class SquidellyController : MonoBehaviour
     public float dmg = 1f;
     public float vision = 5f;
 
+    [SerializeField]
+    private float circleRadius = 3f;
+    [SerializeField]
+    private float circleSpeed = 3f;
+    [SerializeField]
+    private float runSpeed = 1f;
+
     private GameObject player;
     private float playerDistance;
+    private float circleAngle;
+    private bool clockwise;
+
+    // make state machine it go speed kachow
+    // if (hp <= 2) -> else (state machine)
 
     // Start is called before the first frame update
     void Start()
@@ -22,9 +34,48 @@ public class SquidellyController : MonoBehaviour
     {
         playerDistance = Vector3.Distance(gameObject.transform.position, player.transform.position);
 
-        if (playerDistance <= vision)
+        if (hp <= 2)
+        {
+            Debug.Log("Running Away");
+            transform.position = Vector2.MoveTowards(gameObject.transform.position, -1 * player.transform.position, runSpeed * Time.deltaTime);
+        }
+        else if (playerDistance <= circleRadius)
+        {
             Debug.Log("Attacking Player");
+
+            if (clockwise)
+            {
+                transform.RotateAround(player.transform.position, Vector3.forward, -(circleSpeed * Mathf.Rad2Deg) * Time.deltaTime);
+                transform.Rotate(0, 0, (circleSpeed * Mathf.Rad2Deg) * Time.deltaTime);
+            }
+            else
+            {
+                transform.RotateAround(player.transform.position, Vector3.forward, (circleSpeed * Mathf.Rad2Deg) * Time.deltaTime);
+                transform.Rotate(0, 0, -(circleSpeed * Mathf.Rad2Deg) * Time.deltaTime);
+            }
+        }
+        else if (playerDistance <= vision)
+        {
+            Debug.Log("Moving To Player");
+            transform.position = Vector2.MoveTowards(gameObject.transform.position, player.transform.position, runSpeed * Time.deltaTime);
+        }
         else
             Debug.Log("Not Attacking Player");
+
+        // circle player
+        //transform.position = circleRadius * new Vector3(Mathf.Cos(Time.time * speed), Mathf.Sin(Time.time * speed), 0);
+        // run away
+        //transform.position = Vector2.MoveTowards(gameObject.transform.position, -1 * player.transform.position, speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Wall")
+            clockwise = !clockwise;
+    }
+
+    public void takeDamage(float damageTaken)
+    {
+        hp -= damageTaken;
     }
 }

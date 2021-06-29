574b338bc65901c8d498367dd46bf4c9d9f0eaf2
SarzBuild <jrmietn@gmail.com>
2021-06-29 16:29:08 -0400

    Hotfix SandController
    One line was throwing errors since it was null

--- Assets/Scripts/GlassSandController.cs
+++ Assets/Scripts/GlassSandController.cs
@@ -31,7 +31,7 @@ public class GlassSandController : MonoBehaviour
             if (prevTrigger == false)
             {
                 //TODO update this to the new general enemy component once the Squidelly controller is untangled
-                collider.gameObject.GetComponent<SquidellyController>().takeDamage(damagePerSpeed * rb.velocity.magnitude);
+                collider.gameObject.GetComponent<CircleEnemyController>().takeDamage(damagePerSpeed * rb.velocity.magnitude);
             }
         }
 

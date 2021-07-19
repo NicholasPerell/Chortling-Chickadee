e63ea69f70a4fd431eabb1b4d09f4c7f81c739c1
nicholas-hoy-champain <nicholas.hoy@mymail.champlain.edu>
2021-07-19 19:18:51 -0400

    Sand pickup won't spawn if player already has them

--- Assets/Scripts/Environment Elements/Boons/EarningController.cs
+++ Assets/Scripts/Environment Elements/Boons/EarningController.cs
@@ -8,16 +8,21 @@ public class EarningController : MonoBehaviour
     [SerializeField] SandAbilities ability;
     [SerializeField] float sandEarnt;
 
+    PlayerStatsController ply;
+
     private void Start()
     {
         name = "SandProjectileEarn";
+        ply = GameObject.FindObjectOfType<PlayerStatsController>();
+        if (ply.hasAbility[(int)ability])
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     [YarnCommand("Bestow")]
     public void BestowAbility()
     {
-        PlayerStatsController ply = GameObject.FindObjectOfType<PlayerStatsController>();
-
         ply.currentMaxSand = ply.currentMaxSand + sandEarnt;
         ply.hasAbility[(int)ability] = true;
 
